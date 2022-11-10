b430e7f1aea69bf1277e6ca4a7405acd96684994
Leon Kipkoech <leonkipkip@gmail.com>
2022-11-10 16:57:18 -0500

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio';
+  //  @ViewChild('side_nav', { static: true }) sideNav:any;
+  images = [
+    {
+      src: 'https://i.pinimg.com/originals/92/58/77/925877480e6796a0a5a270d56221547b.jpg',
+    },
+    {
+      src: 'https://64.media.tumblr.com/717b96959586e42729749adaa03af521/d2acf56ec54ba323-77/s1280x1920/c3f23bfb7c6a0da289fddb5250240cd009268081.pnj',
+    },
+    {
+      src: 'https://64.media.tumblr.com/6b0fefbb37453001e95cc6868fd1ac2c/d2acf56ec54ba323-90/s1280x1920/925b7233983644c6360b020db2a72fc2da658ccb.pnj',
+    }
+  ]
+  currentImage = 0;
+  expanded: boolean = false;
+   ngOnInit(): void{
+    this.changeImage()
+   }
+  expandSideNav(){
+   this.expanded=!this.expanded
+   console.log(this.expanded)
+  }
+  async changeImage(){
+    while(true){
+      
+        this.currentImage++
+      
+      if(this.currentImage==3){
+        this.currentImage=0
+      }
+      console.log(this.currentImage)
+      await new Promise(resolve => setTimeout(resolve, 3000)); 
+      
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './core/components/card/card.component';
+import { CardParentComponent } from './core/components/card-parent/card-parent.component';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    CardParentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
