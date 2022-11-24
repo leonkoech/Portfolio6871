95f4abda2edbc1de7be0c7fdbca493d3221d00ab
Leon Kipkoech <leonkipkip@gmail.com>
2022-11-23 17:24:23 -0700

    fix typos

--- src/app/core/modules/experience.ts
+++ src/app/core/modules/experience.ts
@@ -7,11 +7,13 @@ export const experiences=[
         location: "Remote",
         title: "Software Development Intern",
         imageUrl: "../assets/images/instrument.svg",
-        details: "I got to experience the full development cycle all the way from conception\
-        to design to development. During my time at instrument, I worked on unlocked.microsoft.com and design.google.com under the guidance of geniuses in their crafts. I also worked on an internship project\
-        which is yet to be released. I also got introduced to accessibility and the parallel development technique.\
+        details: "I got to experience the full development cycle all the way from conceptualization,\
+        to design, to development. I got introduced to web accessibility and the parallel development model which emphasizes inclusivity and diversity.\
+         During my time at instrument, I worked on unlocked.microsoft.com and design.google.com under the guidance of geniuses in their crafts. \
+         I also worked on an internship project which is yet to be released.\
         For unlocked.microsoft.com, I built a component for the front-end to meet some specific specs while maintaining some other components for the CMS. \
-        For design.google.com, I built an automation script with python to minimize the time used in component creation more so because the project used NextJs and an interface for developers to handle project specific types",
+        For design.google.com, I built an automation script with python to minimize the time used in component creation (more so because the project used NextJs/ReactJs) \
+        and an interface for developers to handle project specific types and prevent errors in their code",
     },
     {
         company: "Sustainible",
@@ -21,7 +23,7 @@ export const experiences=[
         location: "Remote",
         title: "Software Development Intern",
         imageUrl: "../assets/images/sustainible.svg",
-        details: "In terms of gaining skills and wearing multiple hats, Sustainible takes the cake. From front-end development to technical direction to backe-end development, I have done them all.\
+        details: "In terms of gaining skills and wearing multiple hats, Sustainible takes the cake. From front-end development to technical direction to back-end development, I have done them all.\
         While at Sustainible, I integrated some API's (stripe,firebase) and helped with others (facebook), created some structure for the project for scalability, core engineering on the structure of data\
         to minimize resources used to read and write data to the backend, created templates for GitHub while reporting directly to the founders\
         ",
